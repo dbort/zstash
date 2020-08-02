44fd35267036aead23fb4fc6c066ed23f75d6f0f
Dave Bort <git@dbort.com>
2020-08-01 20:51:37 -0700

    Split config into its own file

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python3
 
-import collections.abc
-import expandvars
-from gitignore_parser import parse_gitignore
+import backups
+from backups.config import ConfigDict
+import io
 import logging
-import os
-import sys
-import toml
 import typing
-
-# Alias for the config dictionary's type.
-ConfigDict = typing.MutableMapping[str, typing.Any]
-
-# Allowed top-level [section] names.
-VALID_SECTION_NAMES = {'env', 'backups'}
+import sys
 
 raw_config = r'''
 [env]
@@ -44,94 +36,6 @@ ignore="""
 """
 '''
 
-
-class ConfigError(Exception):
-  def __init__(self, message: str):
-    self.message = message
-
-
-def read_config() -> ConfigDict:
-  try:
-    config = toml.loads(raw_config)
-    unknown_sections = set(config.keys()) - VALID_SECTION_NAMES
-    if unknown_sections:
-      raise ConfigError('Unknown section(s): {}'.format(
-        ', '.join([f'[{s}]' for s in unknown_sections])
-      ))
-    return config
-  except toml.decoder.TomlDecodeError as e:
-    raise ConfigError(f'Error while parsing config file: {e}')
-
-
-def expand_vars(config: ConfigDict):
-  """Expands environment variables in config value strings.
-
-  If an 'env' section is present in the config, its keys/values extend the
-  environment during this expansion. All members of the 'env' section must have
-  string values.
-  """
-  def expand_dict(d):
-    """Recursively expands environment vars in all string values in the dict."""
-    for k, v in d.items():
-      if isinstance(v, collections.abc.Mapping):
-        expand_dict(v)
-      elif isinstance(v, str):
-        d[k] = expandvars.expandvars(v, nounset=True)
-        
-  old_env = os.environ.copy()  # Save so we can restore it later.
-  try:
-    # Define env vars specified in the config, whose values may themselves
-    # contain ${vars}. Do them in order in case one uses a value defined before
-    # it.
-    if 'env' in config:
-      for name, value in config.get('env', {}).items():
-        if not isinstance(value, str):
-          raise ConfigError(
-            f'[env] section contains entry "{name}" whose value ' +
-            f'is not a string: {repr(value)}')
-        expanded = expandvars.expandvars(value, nounset=True)
-        config['env'][name] = expanded
-        os.environ[name] = expanded
-      # Don't re-expand these; don't need them anymore anyway.
-      del config['env']
-    # Expand strings in all other sections.
-    expand_dict(config)
-  except expandvars.UnboundVariable as e:
-    raise ConfigError(f'Error while expanding variables: {e}')
-  finally:
-    # Restore the original environment before returning or raising.
-    os.environ.clear()
-    os.environ.update(old_env)
-
-
-def get_backup_configs(config: ConfigDict) -> ConfigDict:
-  """Returns a mapping of [backups.<name>] names to configs.
-
-  Each entry will inherit from but override elements from the top-level
-  [backups] section.
-  """
-  # The root [backups] section, parent of specific backups.
-  root = config.get('backups')
-  if not root:
-    return {}
-
-  # Split [backups] into backups and everything else.
-  backups = {}  # The [backups.*] sub-dicts.
-  base = {}  # `root` without the sub-dicts.
-  for k, v in root.items():
-    if isinstance(v, collections.abc.Mapping):
-      backups[k] = v
-    else:
-      base[k] = v
-
-  # Make backups inherit from (but override) base.
-  for k, v in backups.items():
-    n = base.copy()
-    n.update(v)
-    backups[k] = n
-  return backups
-
-
 def do_backup(name: str, config: ConfigDict, dry_run: bool=False):
   logging.info(f'Preparing backup config [{name}]')
   logging.info(config)
@@ -139,15 +43,8 @@ def do_backup(name: str, config: ConfigDict, dry_run: bool=False):
 
 def main(args: typing.Sequence) -> int:
   #xxx run paths through os.expanduser() for ~
-  try:
-    config = read_config()
-    expand_vars(config)
-  except ConfigError as e:
-    logging.critical(f'Failed while reading config: {e}')
-    return 1
-
-  backups = get_backup_configs(config)
-  for name, backup in backups.items():
+  bb = backups.config.read(io.StringIO(raw_config))
+  for name, backup in bb.items():
     do_backup(name, backup)
   return 0
 
