9aa2fa05526ff3fd3c8d01af5a7ecaff75746451
Dave Bort <git@dbort.com>
2020-08-01 23:39:54 -0700

    file list building looking good

--- backups/runner.py
+++ backups/runner.py
@@ -2,14 +2,25 @@
 
 from backups.config import ConfigDict, BackupConfig
 import logging
+import typing
+import os
+
+def build_file_list(config: BackupConfig) -> typing.Iterable[str]:
+  for root, dirs, files in os.walk(config.src_dir):
+    ignored = [
+        d for d in dirs if config.should_ignore(os.path.join(root, d))
+    ]
+    for i in ignored:
+      dirs.remove(i)
+    for f in files:
+      abs_path = os.path.join(root, f)
+      if not config.should_ignore(abs_path):
+        yield os.path.relpath(abs_path, config.src_dir)
 
 
 def do_backup(config: BackupConfig, dry_run: bool=False):
   logging.info(f'Preparing backup config [{config.name}]')
   logging.info(config.options)
-  p = config.src_dir + '/line1'
-  logging.info(f'should ignore {p}? {config.should_ignore(p)}')
-  p = config.src_dir + '/line10'
-  logging.info(f'should ignore {p}? {config.should_ignore(p)}')
-  p = config.src_dir + '/lineXX'
-  logging.info(f'should ignore {p}? {config.should_ignore(p)}')
+  file_list = build_file_list(config)
+  s = '\n  '.join(file_list)
+  logging.info(f"Files:\n  {s}")
--- main.py
+++ main.py
@@ -17,6 +17,18 @@ log_path="${HOME}/var/log/backup-foundry.log"
 
 # Can also have an "ignore=..." here that will be inherited.
 
+[backups.test]
+s3_subpath="test"
+archive_prefix="test-"
+
+src_dir="${HOME}/src/zipspec"
+ignore="""
+.git
+.mypy_cache
+.pyre*
+__pycache__/
+"""
+
 [backups.worlds]
 s3_subpath="foundry/worlds"
 archive_prefix="foundry-worlds-"
