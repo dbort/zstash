f17eabe4d3872c6cf813bd0f74764310cd1afaeb
Dave Bort <git@dbort.com>
2020-08-01 17:16:26 -0700

    Full env expansion using os.expandvars

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import collections.abc
 from gitignore_parser import parse_gitignore
 import os
 import sys
@@ -43,15 +44,28 @@ def read_config() -> dict:
 
 
 def expand_vars(config: dict):
+  def expand_dict(d):
+    for k, v in d.items():
+      if isinstance(v, collections.abc.Mapping):
+        expand_dict(v)
+      elif isinstance(v, str):
+        d[k] = os.path.expandvars(v)
+        
+
   old_env = os.environ.copy()
   try:
-    # Define vars specified in the config, whose values may themselves
-    # contain ${vars}.
-    for name, value in config.get('env', {}).items():
-      expanded = os.path.expandvars(value)
-      config['env'][name] = expanded
-      os.environ[name] = expanded
+    # Define env vars specified in the config, whose values may themselves
+    # contain ${vars}. Do them in order in case one uses a value defined before
+    # it.
+    if 'env' in config:
+      for name, value in config.get('env', {}).items():
+        expanded = os.path.expandvars(value)
+        config['env'][name] = expanded
+        os.environ[name] = expanded
+      # Don't re-expand these; don't need them anymore anyway.
+      del config['env']
     # Expand strings in all other sections.
+    expand_dict(config)
   finally:
     os.environ.clear()
     os.environ.update(old_env)
